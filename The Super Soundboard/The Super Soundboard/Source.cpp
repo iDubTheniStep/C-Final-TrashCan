e400b1b0a8a7682d53b9a62a8d75540ea997e36f
austinricha15487 <34347111+austinricha15487@users.noreply.github.com>
2018-05-04 13:16:39 -0500

    Update to main
    He's a trashcan

---
+++ The Super Soundboard/The Super Soundboard/Source.cpp
@@ -0,0 +1,34 @@
+// Program: The Super TrashBoard
+// Authors: Austin Richards and Dylan Peiffer
+// Date: May 3 2018
+
+#include <iostream>
+
+#include <random>
+#include <time.h>
+#include <stdlib.h>
+#include <windows.h>
+
+#include <mmsystem.h>
+
+
+#pragma comment(lib, "winmm.lib")
+
+
+int main()
+{	
+	
+
+
+
+	system("pause");
+	return 0;
+}
+
+void darthVader()
+{
+	PlaySound(TEXT("nooo.swf.wav"), NULL, SND_SYNC);
+
+}
+
+
